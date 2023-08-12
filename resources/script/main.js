aff98744f3c9131364e913d14c23f14bfd938cd0
Rubiiee_3 <hammondgeorge92@gmail.com>
2023-08-12 11:40:29 +0000

    Portfolio site redone

--- resources/script/main.js
+++ resources/script/main.js
@@ -1,13 +1,13 @@
-let liChange = document.getElementById("about-me-text");
+let liChange = document.querySelector("main > .about-me-section > .intro-tag-line > #name");
 
 function changeOnOver (){
-liChange.style.backgroundColor= '#40554b';
-liChange.style.color = 'yellow';
+liChange.style.borderBottom= '3px solid #59cdff';
+liChange.style.transition= 'all .35s ease-in-out';
 }
 liChange.onmouseover = changeOnOver;
 
 function changeOutOver (){
-    liChange.style.backgroundColor= 'rgba(13, 39, 95, 0.788)';
+    liChange.style.borderBottom= '#2b2b2b';
     liChange.style.color = 'white';
     }
 
